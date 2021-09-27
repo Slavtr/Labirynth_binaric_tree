c315d3b32e50fbc5c84a54527d24282d9d4e64fe
Бальтазар Лолронов <Slavacer@bk.ru>
2021-09-27 17:23:44 +0300

    Добавлены методы размещения двери и ключа

--- Program.cs
+++ Program.cs
@@ -13,10 +13,13 @@ namespace Лабиринт_Двоичное_дерево
     class Tile
     {
         int type;
-        public int Select_type { get
+        public int Select_type
+        {
+            get
             {
                 return type;
-            } }
+            }
+        }
         public Tile(int type)
         {
             this.type = type;
@@ -31,28 +34,11 @@ namespace Лабиринт_Двоичное_дерево
             y *= 3;
             map = new Tile[x, y];
             int[,] bones = Map_bones(x, y);
-            Random r = new Random();
-            for (int i = 1; i < map.GetLength(0); i++)
-            {
-                for (int j = 1; j < map.GetLength(1)-2; j++)
-                {
-                    if (i == 1) 
-                    {
-                        bones[i, j] = 0;
-                    }
-                    else
-                    {
-                        if (r.Next() % 2 == 0)
-                        {
-                            bones[i - 1, j] = 0;
-                        }
-                        else
-                        {
-                            bones[i, j + 1] = 0;
-                        }
-                    }
-                }
-            }
+            bones = Labirynth_builder(bones);
+            Map_zap(bones);
+        }
+        void Map_zap(int[,] bones)
+        {
             for (int i = 0; i < map.GetLength(0); i++)
             {
                 for (int j = 0; j < map.GetLength(1); j++)
@@ -64,7 +50,7 @@ namespace Лабиринт_Двоичное_дерево
         int[,] Map_bones(int x, int y)
         {
             int[,] ret = new int[x, y];
-            for(int i = 0; i < x; i++)
+            for (int i = 0; i < x; i++)
             {
                 for (int j = 0; j < y; j++)
                 {
@@ -85,15 +71,147 @@ namespace Лабиринт_Двоичное_дерево
         }
         public void Show()
         {
-            for (int i = 0; i < map.GetLength(0); i++) 
+            for (int i = 0; i < map.GetLength(0); i++)
             {
-                for(int j = 0; j < map.GetLength(1); j++)
+                for (int j = 0; j < map.GetLength(1); j++)
                 {
                     Console.Write(map[i, j].Select_type.ToString());
                 }
                 Console.WriteLine();
             }
             Console.WriteLine(map.GetLength(0) + " " + map.GetLength(1));
+        }
+        int[,] Key_place(int[,] bones)
+        {
+            Random r = new Random();
+            int x = r.Next(2, bones.GetLength(0)), y = r.Next(2, bones.GetLength(1));
+            bones[x, y] = 2;
+            while (true)
+            {
+                if (bones[x - 1, y] == 1 && bones[x, y - 1] == 1 && bones[x, y + 1] == 1 && bones[x + 1, y] == 1)
+                {
+                    if (r.Next() % 2 == 0)
+                    {
+                        if (x != bones.GetLength(0) && y != bones.GetLength(1))
+                        {
+                            if (r.Next() % 2 == 0)
+                            {
+                                bones[x + 1, y] = 0;
+                                x++;
+                            }
+                            else
+                            {
+                                bones[x, y + 1] = 0;
+                                y++;
+                            }
+                        }
+                        else
+                        {
+                            if (r.Next() % 2 == 0)
+                            {
+                                bones[x - 1, y] = 0;
+                                x--;
+                            }
+                            else
+                            {
+                                bones[x, y - 1] = 0;
+                                y--;
+                            }
+                        }
+                    }
+                }
+                else break;
+            }
+            return bones;
+        }
+        int[,] Door_place(int[,] bones)
+        {
+            Random r = new Random();
+            int x, y;
+            while (true)
+            {
+                x = r.Next(2, bones.GetLength(0));
+                y = r.Next(2, bones.GetLength(1));
+                if (bones[x, y] != 2)
+                {
+                    bones[x, y] = 3;
+                    break;
+                }
+            }
+            while (true)
+            {
+                if (bones[x - 1, y] == 1 && bones[x, y - 1] == 1 && bones[x, y + 1] == 1 && bones[x + 1, y] == 1)
+                {
+                    if (r.Next() % 2 == 0)
+                    {
+                        if (x != bones.GetLength(0) && y != bones.GetLength(1))
+                        {
+                            if (r.Next() % 2 == 0)
+                            {
+                                bones[x + 1, y] = 0;
+                                x++;
+                            }
+                            else
+                            {
+                                bones[x, y + 1] = 0;
+                                y++;
+                            }
+                        }
+                        else
+                        {
+                            if (r.Next() % 2 == 0)
+                            {
+                                bones[x - 1, y] = 0;
+                                x--;
+                            }
+                            else
+                            {
+                                bones[x, y - 1] = 0;
+                                y--;
+                            }
+                        }
+                    }
+                }
+                else break;
+            }
+            return bones;
+        }
+        int[,] Path_builder(int[,] bones)
+        {
+            bones = Key_place(bones);
+            bones = Door_place(bones);
+            return bones;
+        }
+        int[,] Labirynth_builder(int[,] bones)
+        {
+            Random r = new Random();
+            for (int i = 1; i < map.GetLength(0); i++)
+            {
+                for (int j = 1; j < map.GetLength(1) - 2; j++)
+                {
+                    if (i == 1)
+                    {
+                        bones[i, j] = 0;
+                    }
+                    else
+                    {
+                        if (r.Next() % 2 == 0)
+                        {
+                            bones[i - 1, j] = 0;
+                        }
+                        else
+                        {
+                            bones[i, j + 1] = 0;
+                        }
+                    }
+                }
+            }
+            bones = Path_builder(bones);
+            return bones;
+        }
+        int[,] Path_finder(int[,] bones)
+        {
+
         }
     }
 }
